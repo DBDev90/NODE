ff9573c299812cdac78f2441507fb8729c0eaed9
DBDev90 <bordinassidouglas@gmail.com>
2023-12-13 08:54:32 -0300

    feat: adicionando estrutura de pastas

--- api-rest-node-typescript/src/server/routes/index.ts
+++ api-rest-node-typescript/src/server/routes/index.ts
@@ -3,6 +3,10 @@ import { StatusCodes } from "http-status-codes";
 
 const router = Router();
 
+router.get('/', (_, res) => {
+    return res.send('Conectado!');
+});
+
 router.post('/', (req, res) => {
     console.log(req.body);
     return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
